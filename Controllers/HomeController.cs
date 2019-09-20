539586ca5e479e3108afab1c766fe3a8f57d03c0
jairxx20 <huamanbellidoj@gmail.com>
2019-09-19 19:30:24 -0500

    Se creo la vista de login en la ruta /home/login

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace MiTutor.Controllers
             return View();
         }
 
+        public IActionResult Login(){
+            return View();
+            
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
