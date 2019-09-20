d46bb419b4d044939b96b3e1e271f43e673f63e4
jairxx20 <huamanbellidoj@gmail.com>
2019-09-20 14:19:39 -0500

    Se añadio la vista de registro /home/register

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,6 +25,11 @@ namespace MiTutor.Controllers
             
         }
 
+        public IActionResult Register(){
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
